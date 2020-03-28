7e8d94b6ea0169e83d199b5084c97735e1a44bf1
ikutarian <ikutarian9527@gmail.com>
2020-03-28 17:29:53 +0800

    1. 取消使用mybatis，采用mybatis-plus 2. 修改application.yml中关于mybatis的配置，使用mybatis-plus的配置

--- api/src/main/java/com/ikutarian/Application.java
+++ api/src/main/java/com/ikutarian/Application.java
@@ -1,9 +1,11 @@
 package com.ikutarian;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.ikutarian.mapper")
 public class Application {
 
     public static void main(String[] args) {
